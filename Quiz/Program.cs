f7acfe68fa431be59585983f991433b3688854f9
andrzej-herman <aherman@san.edu.pl>
2024-12-22 18:20:17 +0100

    22.12.2024

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Quiz;
+using QuizLogic;
 
 
 // powołanie obiektów BC
--- QuizLogic/Backend.cs
+++ QuizLogic/Backend.cs
@@ -1,6 +1,7 @@
-﻿using System.Text.Json;
+﻿using Quiz;
+using System.Text.Json;
 
-namespace Quiz
+namespace QuizLogic
 {
     public class Backend
     {
